1d17e70496e23dd38c16c4557bbffb7129abcbba
ColderV3 <glitchbloxg@gmail.com>
2025-07-04 21:59:15 +0200

    Update HudModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/client/HudModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/client/HudModule.java
@@ -1,19 +1,18 @@
-package me.alpha432.insomnia.features.modules.client;
+package me.alpha432.oyvey.features.modules.client;
 
-import me.alpha432.insomnia.Insomnia;
-import me.alpha432.insomnia.event.impl.Render2DEvent;
-import me.alpha432.insomnia.features.modules.Module;
+import me.alpha432.oyvey.OyVey;
+import me.alpha432.oyvey.event.impl.Render2DEvent;
+import me.alpha432.oyvey.features.modules.Module;
 
 public class HudModule extends Module {
     public HudModule() {
-        super("Hud", "hud", Category.CLIENT, true, false, false);
+        super("Hud", "hud", Category.CLIENT, false, false, false);
     }
 
-    @Override
-    public void onRender2D(Render2DEvent event) {
+    @Override public void onRender2D(Render2DEvent event) {
         event.getContext().drawTextWithShadow(
                 mc.textRenderer,
-                Insomnia.NAME + " " + Insomnia.VERSION,
+                OyVey.NAME + " " + OyVey.VERSION,
                 2, 2,
                 -1
         );
